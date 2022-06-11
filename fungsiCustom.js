102084ce87c1eea5db1db2144154631c31c22528
SriWidodo <swidodo87@gmail.com>
2022-06-11 23:55:25 +0800

    Tugas 06 callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,7 +18,68 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = function(){
+
+  const fs = require("fs");
+  fs.readFile(file1,"utf8",(err,data1)=>{
+
+    if(err){
+        console.log("data 1 erro "+ err)
+    }
+    fs.readFile("./data2.json","utf8",(err,data2)=>{
+      if(err){
+          console.log("data 2 erro "+ err)
+      } 
+      fs.readFile("./data3.json","utf8",(err,data3)=>{
+
+        if(err){
+            console.log("data 1 erro "+ err)
+        }
+      
+    let dariDatajson = JSON.parse(data1);
+    let darifilejson = dariDatajson.message;
+    const arrFilejson1 = darifilejson.split(" ");
+    //console.log(arrFilejson1[1]);
+    
+    let dariDatajson2 = JSON.parse(data2);
+    let darifilejson2 = dariDatajson2[0].message;
+    const arrfilejson2= darifilejson2.split(" ")
+    //console.log(arrfilejson2[1]);
+    
+
+    let dariDatajson3 = JSON.parse(data3);
+    let darifilejson3 = dariDatajson3[0].data.message;
+    const arrFilejson3 =darifilejson3.split(" ");
+    //console.log(arrFilejson3[1]);
+    let gabungan= arrFilejson1[1]+','+arrfilejson2[1]+','+arrFilejson3[1];
+    let result = gabungan.split(',');
+    console.log(result);
+   });
+  });
+});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
